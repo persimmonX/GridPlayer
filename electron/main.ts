fdc69152e7d4c0268fe88316e9fc15b7dac62650
xman <xman>
2024-05-10 13:56:12 +0800

    打包配置

--- electron/main.ts
+++ electron/main.ts
@@ -690,7 +690,8 @@ function createWindow() {
   });
 
   if (VITE_DEV_SERVER_URL) {
-    win.loadURL(VITE_DEV_SERVER_URL);
+    win.loadFile(path.join(RENDERER_DIST, "index.html"));
+    // win.loadURL(VITE_DEV_SERVER_URL);
   } else {
     win.loadFile(path.join(RENDERER_DIST, "index.html"));
   }
